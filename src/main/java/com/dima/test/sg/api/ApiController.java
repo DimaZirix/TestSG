48a8eca528a1117dc0c1f1853bcbbcb3946daf25
dima <dima@zirix.com>
2020-02-27 12:30:38 +0000

    чтение дерева документов

--- src/main/java/com/dima/test/sg/api/ApiController.java
+++ src/main/java/com/dima/test/sg/api/ApiController.java
@@ -1,17 +1,39 @@
 package com.dima.test.sg.api;
 
+import com.dima.test.sg.service.Document;
+import com.dima.test.sg.service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
-    public ApiController() {
+    private final DocumentService documentService;
+
+    @Autowired
+    public ApiController(final DocumentService documentService) {
+        this.documentService = documentService;
     }
 
-    @GetMapping("/get/")
-    public void get() {
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Document> get(@PathVariable final long id) {
+        final Document document = documentService.get(id);
 
+        if (document == null) {
+            return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .build();
+        }
+
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(document);
     }
 
     @GetMapping("/add/")
